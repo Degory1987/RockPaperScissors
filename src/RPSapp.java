16fe6f627732006a89b99e2c0da4ef9f9d565c7c
Degory1987 <r.hrubesz@gmail.com>
2020-10-20 22:42:31 +0200

    Languages changes

--- src/RPSapp.java
+++ src/RPSapp.java
@@ -4,47 +4,46 @@ import java.util.Scanner;
 public class RPSapp {
     public static void main(String[] args) {
         // Player choice one of:
-        System.out.println("Witaj, podaj co wybierasz: 1- kamień, 2 - papier, 3 - nożyce");
+        System.out.println("Hello! Make your choice: 1- ROKC, 2 - PAPER, 3 - SCISSORS");
 
         Scanner scanner = new Scanner(System.in);
         int chooseNumb = scanner.nextInt();
 
-        System.out.print("gracz: ");
-        metoda(chooseNumb);
+        System.out.print("Player: ");
+        makeChoice(chooseNumb);
 
         // computer random choice
         Random r = new Random();
         int comNumb = r.nextInt(3);
-        System.out.print("komputer: ");
-        metoda(comNumb);
+        System.out.print("Computer: ");
+        makeChoice(comNumb);
 
 
         if (chooseNumb == 1 && comNumb == 3 || chooseNumb == 2 && comNumb == 1 || chooseNumb == 3 && comNumb == 2)
-            System.out.println("\nWygrałeś");
+            System.out.println("\nYou won!");
         if (chooseNumb == 1 && comNumb == 2 || chooseNumb == 2 && comNumb == 3 || chooseNumb == 3 && comNumb == 1)
-            System.out.println("\nPrzegrywasz");
+            System.out.println("\nYou loose.");
         if (chooseNumb == 1 && comNumb == 1 || chooseNumb == 2 && comNumb == 2 || chooseNumb == 3 && comNumb == 3)
-            System.out.println("\nremis");
+            System.out.println("\nTie.");
 
     }
 
 
-    static int metoda(int n) {
+    static void makeChoice(int n) {
 
         switch (n) {
             case 1:
-                System.out.println("kamień");
+                System.out.println("ROCK!");
                 break;
             case 2:
-                System.out.println("papier");
+                System.out.println("PAPER!");
                 break;
             case 3:
-                System.out.println("nożce");
+                System.out.println("SCISSORS");
                 break;
 
             default:
         }
-        return n;
     }
 
 }
