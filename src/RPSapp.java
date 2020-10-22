865c493da0341210e3fc9c09e1990445dc67e176
Degory1987 <r.hrubesz@gmail.com>
2020-10-22 22:26:47 +0200

    Add game points, repare computer choise problem - random int range.

--- src/RPSapp.java
+++ src/RPSapp.java
@@ -3,29 +3,51 @@ import java.util.Scanner;
 
 public class RPSapp {
     public static void main(String[] args) {
-        // Player choice one of:
-        System.out.println("Hello! Make your choice: 1- ROKC, 2 - PAPER, 3 - SCISSORS");
 
+        System.out.println("Enter your name:");
         Scanner scanner = new Scanner(System.in);
-        int chooseNumb = scanner.nextInt();
+        String name = scanner.nextLine();
+        System.out.println("Hello! " + name + ".\n");
 
-        System.out.print("Player: ");
-        makeChoice(chooseNumb);
+        int playerPoints = 0;
+        int computerPoints = 0;
 
-        // computer random choice
-        Random r = new Random();
-        int comNumb = r.nextInt(3);
-        System.out.print("Computer: ");
-        makeChoice(comNumb);
+        do {
+            // Player choice one of:
+            System.out.print("Make your choice: 1- ROCK, 2 - PAPER, 3 - SCISSORS \n");
+            int chooseNumb = scanner.nextInt();
+
+            System.out.print(name + " ");
+            makeChoice(chooseNumb);
+
+            // computer random choice
+            Random r = new Random();
+            int comNumb = r.nextInt(3) + 1; // Random().nextInt(int bound) = Random integer from 0 (inclusive) to bound (exclusive).// r.nextInt(3) + 1 makes range from 1 to 3.
+
+            System.out.print("Computer ");
+            makeChoice(comNumb);
 
 
-        if (chooseNumb == 1 && comNumb == 3 || chooseNumb == 2 && comNumb == 1 || chooseNumb == 3 && comNumb == 2)
-            System.out.println("\nYou won!");
-        if (chooseNumb == 1 && comNumb == 2 || chooseNumb == 2 && comNumb == 3 || chooseNumb == 3 && comNumb == 1)
-            System.out.println("\nYou loose.");
-        if (chooseNumb == 1 && comNumb == 1 || chooseNumb == 2 && comNumb == 2 || chooseNumb == 3 && comNumb == 3)
-            System.out.println("\nTie.");
+            if (chooseNumb == 1 && comNumb == 3 || chooseNumb == 2 && comNumb == 1 || chooseNumb == 3 && comNumb == 2) {
+                System.out.println("\n" + name + " point for you!");
+                playerPoints++;
+            }
+            if (chooseNumb == 1 && comNumb == 2 || chooseNumb == 2 && comNumb == 3 || chooseNumb == 3 && comNumb == 1) {
+                System.out.println("\nPoint for computer.");
+                computerPoints++;
+            }
+            if (chooseNumb == 1 && comNumb == 1 || chooseNumb == 2 && comNumb == 2 || chooseNumb == 3 && comNumb == 3)
+                System.out.println("\nIt's a tie!");
 
+            System.out.println("\nResult:");
+            System.out.println(name + " " + playerPoints);
+            System.out.println("Computer " + computerPoints +"\n");
+
+        } while (playerPoints < 3 && computerPoints < 3); // zly warunek???
+
+        if (playerPoints == 3) System.out.println("\n" + name + " You won!");
+        else System.out.println(name + " you loose:(");
+        scanner.close();
     }
 
 
@@ -39,7 +61,7 @@ public class RPSapp {
                 System.out.println("PAPER!");
                 break;
             case 3:
-                System.out.println("SCISSORS");
+                System.out.println("SCISSORS!");
                 break;
 
             default:
