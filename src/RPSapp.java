e91c3b194382051a6fda703eaa76791943efdb5c
Degory1987 <r.hrubesz@gmail.com>
2020-10-27 20:11:06 +0100

    attempting to add an option to play again

--- src/RPSapp.java
+++ src/RPSapp.java
@@ -11,44 +11,59 @@ public class RPSapp {
 
         int playerPoints = 0;
         int computerPoints = 0;
+        String s ;
 
         do {
-            // Player choice one of:
-            System.out.print("Make your choice: 1- ROCK, 2 - PAPER, 3 - SCISSORS \n");
-            int chooseNumb = scanner.nextInt();
 
-            System.out.print(name + " ");
-            makeChoice(chooseNumb);
+            do {
+                // Player choice one of:
+                System.out.print("Make your choice: 1- ROCK, 2 - PAPER, 3 - SCISSORS \n");
+                int chooseNumb = scanner.nextInt();
 
-            // computer random choice
-            Random r = new Random();
-            int comNumb = r.nextInt(3) + 1; // Random().nextInt(int bound) = Random integer from 0 (inclusive) to bound (exclusive).// r.nextInt(3) + 1 makes range from 1 to 3.
+                System.out.print(name + " ");
+                makeChoice(chooseNumb);
 
-            System.out.print("Computer ");
-            makeChoice(comNumb);
+                // computer random choice
+                Random r = new Random();
+                int comNumb = r.nextInt(3) + 1; // Random().nextInt(int bound) = Random integer from 0 (inclusive) to bound (exclusive).// r.nextInt(3) + 1 makes range from 1 to 3.
+
+                System.out.print("Computer ");
+                makeChoice(comNumb);
 
 
-            if (chooseNumb == 1 && comNumb == 3 || chooseNumb == 2 && comNumb == 1 || chooseNumb == 3 && comNumb == 2) {
-                System.out.println("\n" + name + " point for you!");
-                playerPoints++;
+                if (chooseNumb == 1 && comNumb == 3 || chooseNumb == 2 && comNumb == 1 || chooseNumb == 3 && comNumb == 2) {
+                    System.out.println("\n" + name + " point for you!");
+                    playerPoints++;
+                }
+                if (chooseNumb == 1 && comNumb == 2 || chooseNumb == 2 && comNumb == 3 || chooseNumb == 3 && comNumb == 1) {
+                    System.out.println("\nPoint for computer.");
+                    computerPoints++;
+                }
+                if (chooseNumb == 1 && comNumb == 1 || chooseNumb == 2 && comNumb == 2 || chooseNumb == 3 && comNumb == 3)
+                    System.out.println("\nIt's a tie!");
+
+                System.out.println("\nResult:");
+                System.out.println(name + " " + playerPoints);
+                System.out.println("Computer " + computerPoints + "\n");
+
+            } while (playerPoints < 3 && computerPoints < 3);
+
+            if (playerPoints == 3) {
+                System.out.println("\n" + name + " You won!");
+                System.out.println("Do you want to play again? Y/N");
+            } else {
+                System.out.println(name + " you loose:(");
+                System.out.println("Do you want to play again? Y/N");
             }
-            if (chooseNumb == 1 && comNumb == 2 || chooseNumb == 2 && comNumb == 3 || chooseNumb == 3 && comNumb == 1) {
-                System.out.println("\nPoint for computer.");
-                computerPoints++;
-            }
-            if (chooseNumb == 1 && comNumb == 1 || chooseNumb == 2 && comNumb == 2 || chooseNumb == 3 && comNumb == 3)
-                System.out.println("\nIt's a tie!");
 
-            System.out.println("\nResult:");
-            System.out.println(name + " " + playerPoints);
-            System.out.println("Computer " + computerPoints +"\n");
+            s = scanner.nextLine();
 
-        } while (playerPoints < 3 && computerPoints < 3); // zly warunek???
+        } while (s.contentEquals("y"));
 
-        if (playerPoints == 3) System.out.println("\n" + name + " You won!");
-        else System.out.println(name + " you loose:(");
-        scanner.close();
+
+        System.out.println("Thank you for your game!");
     }
+
 
 
     static void makeChoice(int n) {
