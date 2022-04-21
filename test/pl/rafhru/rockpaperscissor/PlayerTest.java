ee363e9a21f2b46367fc02f75f967035642961d8
Rafał <r.hrubesz@gmail.com>
2022-04-21 22:44:16 +0200

    update .gitignore

---
+++ test/pl/rafhru/rockpaperscissor/PlayerTest.java
@@ -0,0 +1,13 @@
+package pl.rafhru.rockpaperscissor;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayerTest {
+
+    @Test
+    void makeChoice() {
+
+    }
+}
